b194872510beb3f7643b4c71d63c8dd9ef9bf0c2
Anton Repin <dire.one@gmail.com>
2019-05-21 21:22:46 -0700

    Main file updated and configuration default updated

--- app/migrations/1000000000001-create.database.migration.ts
+++ app/migrations/1000000000001-create.database.migration.ts
@@ -1,16 +1,12 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-console.log(Date.now());
-
-export class CreateDatabase1000001 implements MigrationInterface {
+export class CreateDatabase1000000000001 implements MigrationInterface {
 
     async down(queryRunner: QueryRunner): Promise<any> {
-        console.log("RUNNING DOWN");
         await queryRunner.dropDatabase("proto-service");
     }
 
     async up(queryRunner: QueryRunner): Promise<any> {
-        console.log("RUNNING UP");
         await queryRunner.createDatabase("proto-service");
     }
 
